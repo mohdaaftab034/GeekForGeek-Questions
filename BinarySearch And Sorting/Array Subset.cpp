090f4e2f5fb928df4ceee6669fc1d7299f8a7a67
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-03-01 00:59:49 +0530

    Create Array Subset.cpp

---
+++ BinarySearch And Sorting/Array Subset.cpp
@@ -0,0 +1,24 @@
+// GFG Link - https://www.geeksforgeeks.org/problems/array-subset-of-another-array2317/1?utm=codolio
+
+
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        unordered_map<int, int> freq;
+        
+        for(int num: a) {
+            freq[num]++;
+        }
+        
+        for(int num: b) {
+            if(freq[num] == 0) { // element is not present in A
+                return false;
+            }
+            
+            freq[num]--;
+        }
+        
+        return true;
+    }
+};
