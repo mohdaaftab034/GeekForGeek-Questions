c9bb4ce116723d8435479e812b2aef28b134a2b9
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-04-14 13:18:50 +0530

    Implement nthStair method to count ways to Nth stair

---
+++ Dynamic Programming /Count ways to N'th Stair(Order does not matter).cpp
@@ -0,0 +1,12 @@
+// GFG - https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter5639/1?utm=codolio
+
+
+
+class Solution {
+  public:
+    int nthStair(int n) {
+        return (n/2) + 1;
+    }
+};
+
+
